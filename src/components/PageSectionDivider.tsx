2161ed47f5dc892272673227d96f86217df2a054
Abiraman K <abiramancit@gmail.com>
2023-04-18 17:22:01 +0530

    Adds style props in PageSectionDivider

--- src/components/PageSectionDivider.tsx
+++ src/components/PageSectionDivider.tsx
@@ -1,16 +1,17 @@
 import React from "react";
-import { Divider } from "@mui/material";
+import { Divider, DividerProps } from "@mui/material";
 
 export interface IPageSectionDividerProps {
   orientation?: "horizontal" | "vertical";
   children?: string | React.ReactNode;
+  style?: Pick<DividerProps, "sx">["sx"];
 }
 
 export function PageSectionDivider(props: IPageSectionDividerProps) {
   return (
     <Divider
-      sx={
-        props?.children
+      sx={{
+        ...(props?.children
           ? {
               color: "rgb(255, 255, 255, 0.6)",
               backgroundColor: "rgb(35,35,35)",
@@ -24,8 +25,9 @@ export function PageSectionDivider(props: IPageSectionDividerProps) {
           : {
               minHeight: "0.6rem",
               backgroundColor: "rgb(35,35,35)",
-            }
-      }
+            }),
+        ...props?.style,
+      }}
       orientation={props?.orientation}
     >
       {props?.children}
