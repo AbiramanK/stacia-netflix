da75d61fd5434357ba6c627c41dc173c0e02fca5
Abiraman K <abiramancit@gmail.com>
2023-04-20 11:47:17 +0530

    Adds numberOfLines props & implementation in PageBody1

--- src/components/PageBody1.tsx
+++ src/components/PageBody1.tsx
@@ -7,13 +7,29 @@ export interface IPageBody1Props {
   children: React.ReactNode;
   style?: TypographyStyle;
   variant?: TypographyVariant;
+  numberOfLines?: number;
 }
 
 export function PageBody1(props: IPageBody1Props) {
+  const numberOfLinesStyle: React.CSSProperties = {
+    display: "-webkit-box",
+    overflow: "hidden",
+    WebkitBoxOrient: "vertical",
+    WebkitLineClamp: props?.numberOfLines!,
+  };
+
   return (
     <Typography
       variant={props?.variant ?? "body1"}
-      sx={{ fontSize: "1rem", color: Colors?.common?.white, ...props?.style }}
+      sx={[
+        props?.numberOfLines! ? numberOfLinesStyle : {},
+        {
+          fontSize: "1rem",
+          color: Colors?.common?.white,
+
+          ...props?.style,
+        },
+      ]}
     >
       {props?.children}
     </Typography>
