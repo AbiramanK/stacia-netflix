8141bf84dedce83d6c5f9b573f3c185643b0a58e
Abiraman K <abiramancit@gmail.com>
2023-04-20 11:50:44 +0530

    Refactors PageCategory & Adds option to categorize the movies

--- src/components/PageCategory.tsx
+++ src/components/PageCategory.tsx
@@ -1,21 +1,40 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import {
-  Card,
+  AppBar,
+  Dialog,
   Grid,
   GridProps,
   IconButton,
   List,
   ListItem,
-  ListItemButton,
   ListProps,
+  Toolbar,
+  Typography,
+  Slide,
+  Autocomplete,
+  TextField,
 } from "@mui/material";
-import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
+import { TransitionProps } from "@mui/material/transitions";
+import AddIcon from "@mui/icons-material/Add";
+import CloseIcon from "@mui/icons-material/Close";
 
 import PageDescription1 from "./PageDescription1";
-import PageBody1 from "./PageBody1";
+import MoviesList from "./MoviesList";
+import MovieCard from "./MovieCard";
+import { categories } from "src/constants/data";
 
-const scrollNumberOfItems = 4;
+const scrollNumberOfItems = 5;
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement;
+  },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 export interface IPageCategoryProps {
   category: CategoryInterface;
@@ -25,29 +44,25 @@ export interface IPageCategoryProps {
   categoryContainerStyle?: Pick<GridProps, "sx">["sx"];
   categoryListStyle?: Pick<ListProps, "sx">["sx"];
   categoryGridContainerStyle?: Pick<GridProps, "sx">["sx"];
+  onAddClick?: (category: CategoryInterface) => void;
 }
 
 export function PageCategory(props: IPageCategoryProps) {
+  const { t } = useTranslation();
   const navigate = useNavigate();
 
-  const [activeIndex, setActiveIndex] = useState<number>();
+  const [openOpDialog, setOpenOpDialog] = useState<boolean>(false);
+  const [selectedCategory, setSelectedCategory] = useState<CategoryInterface>(
+    categories[0]
+  );
 
-  function onLeftArrowClick() {
-    setActiveIndex((state) =>
-      (state ?? 0) < scrollNumberOfItems
-        ? 0
-        : (state ?? scrollNumberOfItems) - scrollNumberOfItems
-    );
-  }
-
-  function onRightArrowClick() {
-    const listLength = props?.list?.length;
-
-    setActiveIndex((state) =>
-      (state ?? 0) + scrollNumberOfItems > listLength - 1
-        ? listLength - 1
-        : (state ?? 0) + scrollNumberOfItems
-    );
+  function onAddClick(category: CategoryInterface) {
+    if (props?.onAddClick!) {
+      props?.onAddClick(props?.category);
+    } else {
+      setSelectedCategory(category);
+      onOpDialogOpen();
+    }
   }
 
   function onTitleClick(movie: MovieInterface) {
@@ -58,82 +73,98 @@ export function PageCategory(props: IPageCategoryProps) {
     });
   }
 
+  function onOpDialogOpen() {
+    setOpenOpDialog(true);
+  }
+
+  function onOpDialogClose() {
+    setOpenOpDialog(false);
+  }
+
   return (
     <Grid container rowGap={1} sx={{ ...props?.containerStyle }}>
-      <Grid item sx={{ pl: 5 }}>
-        <PageDescription1>{props?.category?.name}</PageDescription1>
-      </Grid>
-      <Grid item xs={12}>
-        <Grid
-          container
-          alignItems={"center"}
-          justifyContent={"center"}
-          sx={{ ...props?.categoryContainerStyle }}
-        >
-          <Grid item xs={0.3}>
-            <Grid container justifyContent={"flex-start"}>
-              <Grid item>
-                <IconButton sx={{ p: 0 }} onClick={onLeftArrowClick}>
-                  <ArrowBackIos />
-                </IconButton>
-              </Grid>
-            </Grid>
+      <Grid item xs={12} sx={{ paddingInline: 5 }}>
+        <Grid container justifyContent={"space-between"} alignItems={"center"}>
+          <Grid item>
+            <PageDescription1>{props?.category?.name}</PageDescription1>
           </Grid>
-          <Grid item xs={11.2}>
-            <List
-              id="category-list-container"
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "flex-start",
-                flexWrap: "nowrap",
-                overflowX: "scroll",
-                scrollBehavior: "smooth",
-                ...props?.categoryListStyle,
-              }}
-            >
-              {props?.list?.map((movie, index) => (
-                <ListItem key={index} sx={{ p: 0 }}>
-                  <ListItemButton
-                    autoFocus={index === activeIndex}
-                    onClick={() => onTitleClick(movie)}
-                  >
-                    <Grid
-                      container
-                      flexDirection={"column"}
-                      alignItems={"center"}
-                      rowGap={1}
-                      sx={{ ...props?.categoryGridContainerStyle }}
-                    >
-                      <Grid item>
-                        <Card>
-                          <img
-                            src={movie?.thumbnail}
-                            alt={movie?.name}
-                            style={{ verticalAlign: "middle" }}
-                          />
-                        </Card>
-                      </Grid>
-                      <Grid item>
-                        <PageBody1>{movie?.name}</PageBody1>
-                      </Grid>
-                    </Grid>
-                  </ListItemButton>
-                </ListItem>
-              ))}
-            </List>
-          </Grid>
-          <Grid item xs={0.3}>
-            <Grid container justifyContent={"flex-end"}>
-              <Grid item>
-                <IconButton sx={{ p: 0 }} onClick={onRightArrowClick}>
-                  <ArrowForwardIos />
-                </IconButton>
-              </Grid>
-            </Grid>
+          <Grid item>
+            <IconButton onClick={() => onAddClick(props?.category)}>
+              <AddIcon sx={{ fontSize: 28 }} />
+            </IconButton>
           </Grid>
         </Grid>
       </Grid>
+      <Grid item xs={12}>
+        <MoviesList
+          containerStyle={props?.categoryContainerStyle}
+          listStyle={props?.categoryListStyle}
+          gridContainerStyle={props?.categoryGridContainerStyle}
+          scrollNumberOfItems={scrollNumberOfItems}
+          list={props?.list}
+          onTitleClick={onTitleClick}
+        />
+      </Grid>
+      <Dialog
+        fullScreen
+        open={openOpDialog}
+        onClose={onOpDialogClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar sx={{ position: "relative" }}>
+          <Toolbar>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              Movies
+            </Typography>
+
+            <IconButton
+              edge="end"
+              color="inherit"
+              onClick={onOpDialogClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Autocomplete
+          disablePortal
+          id="search-box"
+          options={categories}
+          getOptionLabel={(option) => option?.name}
+          value={selectedCategory}
+          sx={{
+            width: 300,
+            mt: 3,
+            ml: 6,
+          }}
+          size={"medium"}
+          renderInput={(params) => (
+            <TextField {...params} label={t("common.inputs.category")} />
+          )}
+        />
+        <List
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            rowGap: 2,
+            mt: 2,
+          }}
+        >
+          {props?.list?.map((movie, index) => (
+            <ListItem key={index} sx={{ p: 0, width: 285 }}>
+              <MovieCard
+                image={movie?.thumbnail}
+                title={movie?.name}
+                selectable
+                selected
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Dialog>
     </Grid>
   );
 }
